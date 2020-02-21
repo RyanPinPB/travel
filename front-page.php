4e3c6863791637a28bec1c54e1141f0d92efce66
Ryan Pearson <ryanpinpb@gmail.com>
2020-02-21 15:47:55 -0800

    first commit

---
+++ front-page.php
@@ -0,0 +1,151 @@
+<?php get_header() ?>
+
+<section class="bio-overlay">
+    <div class="bio-container">
+        <div class="bio bourdain">
+            <div class="bio-image"></div>
+            <div class="bio-info">
+                <h2 class="bio-name">Anthony Bourdain</h2>
+                <p class="bio-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lacus velit, venenatis sed orci a, sollicitudin sagittis orci. Curabitur a nisi at velit consequat tempor vel in leo. Aliquam erat volutpat. Donec semper ante massa, et mollis est sollicitudin quis. Pellentesque faucibus lacus vitae tellus porta facilisis. Vestibulum porttitor accumsan eros vitae eleifend. Sed commodo efficitur risus at volutpat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur.</p>
+                <button class="close-bio">CLOSE BIO</button>
+            </div>
+        </div>
+        <div class="bio huang">
+            <div class="bio-image"></div>
+            <div class="bio-info">
+                <h2 class="bio-name">Eddie Huang</h2>
+                <p class="bio-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lacus velit, venenatis sed orci a, sollicitudin sagittis orci. Curabitur a nisi at velit consequat tempor vel in leo. Aliquam erat volutpat. Donec semper ante massa, et mollis est sollicitudin quis. Pellentesque faucibus lacus vitae tellus porta facilisis. Vestibulum porttitor accumsan eros vitae eleifend. Sed commodo efficitur risus at volutpat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur.</p>
+                <button class="close-bio">CLOSE BIO</button>
+            </div>
+        </div>
+        <div class="bio bronson">
+            <div class="bio-image"></div>
+            <div class="bio-info">
+                <h2 class="bio-name">Action Bronson</h2>
+                <p class="bio-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lacus velit, venenatis sed orci a, sollicitudin sagittis orci. Curabitur a nisi at velit consequat tempor vel in leo. Aliquam erat volutpat. Donec semper ante massa, et mollis est sollicitudin quis. Pellentesque faucibus lacus vitae tellus porta facilisis. Vestibulum porttitor accumsan eros vitae eleifend. Sed commodo efficitur risus at volutpat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur.</p>
+                <button class="close-bio">CLOSE BIO</button>
+            </div>
+        </div>
+        <div class="bio neistat">
+            <div class="bio-image"></div>
+            <div class="bio-info">
+                <h2 class="bio-name">Casey Neistat</h2>
+                <p class="bio-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lacus velit, venenatis sed orci a, sollicitudin sagittis orci. Curabitur a nisi at velit consequat tempor vel in leo. Aliquam erat volutpat. Donec semper ante massa, et mollis est sollicitudin quis. Pellentesque faucibus lacus vitae tellus porta facilisis. Vestibulum porttitor accumsan eros vitae eleifend. Sed commodo efficitur risus at volutpat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur.</p>
+                <button class="close-bio">CLOSE BIO</button>
+            </div>
+        </div>
+    </div>
+</section>
+
+<main class="main">
+    <section class="hero">
+        <div class="background">
+            <h2 class="hero-text section-title">Adventure</h2>
+            <div class="dude"></div>
+            <div class="fade-to-black"></div>
+        </div>
+    </section>
+    <div class="wrap">
+        <section id="people" class="people">
+            <h2 class="section-title">People</h2>
+            <div class="carousel-container">
+                <div class="customNavigation">
+                    <span class="nav-title">TRAVELERS</span>
+                    <button type="button" role="presentation" class="people-prev"> <span class="btn prev" aria-label="Previous">‹</span></button>
+                    <button type="button" role="presentation" class="people-next"> <span class="btn next" aria-label="Next">›</span></button>
+                </div>
+
+                <div id="owl-people" class="owl-carousel owl-theme">
+                    <div class="item person">
+                        <div class="person-image bourdain"></div>
+                        <div class="person-info">
+                            <div class="person-name">Anthony Bourdain</div>
+                            <button id="open-bourdain" class="view-bio">VIEW BIO</button>
+                        </div>
+                    </div>
+                    <div class="item person">
+                        <div class="person-image huang"></div>
+                        <div class="person-info">
+                            <div class="person-name">Eddie Huang</div>
+                            <button id="open-huang" class="view-bio">VIEW BIO</button>
+                        </div>
+                    </div>
+                    <div class="item person">
+                        <div class="person-image bronson"></div>
+                        <div class="person-info">
+                            <div class="person-name">Action Bronson</div>
+                            <button id="open-bronson" class="view-bio">VIEW BIO</button>
+                        </div>
+                    </div>
+                    <div class="item person">
+                        <div class="person-image neistat"></div>
+                        <div class="person-info">
+                            <div class="person-name">Casey Neistat</div>
+                            <button id="open-neistat" class="view-bio">VIEW BIO</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="places" class="places">
+            <h2 class="section-title">Places</h2>
+            <div class="location">
+                <img class="location-picture antelope slide-up" src="<?php echo get_template_directory_uri(); ?>/assets/images/home/canyon.jpg" alt="Antelope Canyon"/>
+                <h2 class="location-name antelope slide-up">Antelope Canyon</h2>
+                <p class="location-description antelope slide-up">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lacus velit, venenatis sed orci a, sollicitudin sagittis orci. Curabitur a nisi at velit consequat tempor vel in leo. Aliquam erat volutpat. Donec semper ante massa, et mollis est sollicitudin quis. Pellentesque faucibus lacus vitae tellus porta facilisis. Vestibulum porttitor accumsan eros vitae eleifend.</p>
+            </div>
+            <div class="location">
+                <img class="location-picture torres slide-up" src="<?php echo get_template_directory_uri(); ?>/assets/images/home/lake.jpg" alt="Torres Del Paine lake and mountains"/>
+                <h2 class="location-name torres slide-up">Torres Del Paine</h2>
+                <p class="location-description torres slide-up">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lacus velit, venenatis sed orci a, sollicitudin sagittis orci. Curabitur a nisi at velit consequat tempor vel in leo. Aliquam erat volutpat. Donec semper ante massa, et mollis est sollicitudin quis. Pellentesque faucibus lacus vitae tellus porta facilisis. Vestibulum porttitor accumsan eros vitae eleifend.</p>
+            </div>
+        </section>
+
+        <section id="gallery" class="gallery">
+            <h2 class="section-title">Gallery</h2>
+            <div class="customNavigation">
+                <button type="button" role="presentation" class="gallery-prev"> <span class="btn prev" aria-label="Previous">‹</span></button>
+                <button type="button" role="presentation" class="gallery-next"> <span class="btn next" aria-label="Next">›</span></button>
+            </div>
+            <div id="owl-gallery" class="owl-carousel owl-theme">
+                <div class="item gallery">
+                    <img class="owl-lazy" data-src="<?php echo get_template_directory_uri(); ?>/assets/images/home/santorini.jpg" alt="santorini buildings" />
+                </div>
+                <div class="item gallery">
+                    <img class="owl-lazy" data-src="<?php echo get_template_directory_uri(); ?>/assets/images/home/fuji.jpg" alt="mount fuji" />
+                </div>
+                <div class="item gallery">
+                    <img class="owl-lazy" data-src="<?php echo get_template_directory_uri(); ?>/assets/images/home/moscow.jpg" alt="moscow building" />
+                </div>
+            </div>
+        </section>
+    </div>
+</main>
+
+<aside>
+    <div class="social-container">
+        <span class="share">SHARE</span>
+        <a class="social-icon facebook" href="https://www.facebook.com/PowerDigitalMarketing/" target="blank" aria-label="visit Power Digital's facebook page">
+            <svg class="facebook" alt="Facebook icon" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9.321 20"><title>Facebook</title>
+                <path id="Path_26" data-name="Path 26" class="cls-1" d="M131.821,6.553V4.83a3.8,3.8,0,0,1,.036-.6,1.167,1.167,0,0,1,.164-.419.669.669,0,0,1,.413-.285,2.938,2.938,0,0,1,.759-.079h1.723V0h-2.755a4.484,4.484,0,0,0-3.435,1.135,4.78,4.78,0,0,0-1.044,3.344V6.553h-2.063V10h2.063V20h4.139V10h2.755l.364-3.447Z" transform="translate(-125.619)"/>
+            </svg>
+        </a>
+        <a class="social-icon twitter" href="https://twitter.com/pwrdigmarketing?lang=en" target="blank" aria-label="visit Power Digital twitter page">
+            <svg class="twitter" alt="Twitter icon" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17.1 14"><title>Twitter</title>
+                <g id="twitter" transform="translate(0.002 -28.097)">
+                    <path id="XMLID_827_" class="cls-1" d="M16.71,29.713a6.482,6.482,0,0,1-.827.3,3.656,3.656,0,0,0,.744-1.309.276.276,0,0,0-.4-.321,6.509,6.509,0,0,1-1.924.76,3.672,3.672,0,0,0-6.24,2.624,3.744,3.744,0,0,0,.03.474,9.4,9.4,0,0,1-6.45-3.422.276.276,0,0,0-.452.035,3.676,3.676,0,0,0,.376,4.225,3.109,3.109,0,0,1-.491-.219.276.276,0,0,0-.41.235c0,.016,0,.033,0,.049a3.684,3.684,0,0,0,1.8,3.157c-.093-.009-.187-.023-.279-.041a.276.276,0,0,0-.314.355,3.67,3.67,0,0,0,2.689,2.46,6.494,6.494,0,0,1-3.471.991,6.64,6.64,0,0,1-.777-.046.276.276,0,0,0-.181.506A9.9,9.9,0,0,0,5.478,42.1a9.523,9.523,0,0,0,7.378-3.241,10.272,10.272,0,0,0,2.557-6.695c0-.1,0-.2,0-.3a7.136,7.136,0,0,0,1.642-1.74.276.276,0,0,0-.341-.405Z" transform="translate(0 0)"/>
+                </g>
+            </svg>
+        </a>
+        <a class="social-icon pinterest" href="https://pinterest.com" target="blank" aria-label="visit pinterest">
+            <svg class="pinterest" alt="Pinterest icon" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14.147 18"><title>Pinterest</title>
+                <g id="pinterest" transform="translate(-33.181)">
+                    <path id="XMLID_799_" class="cls-1" d="M45.494,1.845A6.9,6.9,0,0,0,40.665,0a7.589,7.589,0,0,0-5.6,2.158,6.377,6.377,0,0,0-1.887,4.42c0,2,.838,3.542,2.242,4.114a.742.742,0,0,0,.282.058.627.627,0,0,0,.612-.5c.047-.178.157-.618.2-.809a.759.759,0,0,0-.2-.821,2.616,2.616,0,0,1-.595-1.787A4.5,4.5,0,0,1,40.388,2.3c2.4,0,3.9,1.366,3.9,3.566a7.41,7.41,0,0,1-.842,3.62,2.49,2.49,0,0,1-2.06,1.441,1.365,1.365,0,0,1-1.086-.5,1.249,1.249,0,0,1-.219-1.085c.107-.454.253-.927.395-1.384a8.566,8.566,0,0,0,.5-2.254,1.637,1.637,0,0,0-1.647-1.8c-1.252,0-2.233,1.272-2.233,2.9a4.305,4.305,0,0,0,.307,1.62c-.158.668-1.1,4.642-1.273,5.391-.1.437-.722,3.892.3,4.167,1.152.309,2.181-3.055,2.286-3.435.085-.309.382-1.478.564-2.2a3.482,3.482,0,0,0,2.323.9,5.18,5.18,0,0,0,4.159-2.081,8.353,8.353,0,0,0,1.564-5.108A5.89,5.89,0,0,0,45.494,1.845Z" transform="translate(0 0)"/>
+                </g>
+            </svg>
+        </a>
+    </div>
+</aside>
+
+<?php get_footer() ?>
---
+++ header.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package travel
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<!-- insert Google Analytics upon creation -->
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<!-- insert more Meta Tags OR (preferred) install/optimize Yoast Plugin -->
+	<meta name="author" content="Ryan Pearson">
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+    <div id="loader-wrapper">
+        <div id="loader">
+			<svg class="loader-logo" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+		width="556.568px" height="556.568px" viewBox="0 0 556.568 556.568" style="enable-background:new 0 0 556.568 556.568;"
+		xml:space="preserve">
+				<g>
+					<g>
+						<path d="M210.37,133.725V43.434c0-2.264,0.116-4.501,0.333-6.708C97.48,66.592,13.985,169.665,13.985,292.27
+							c0,145.969,118.33,264.299,264.298,264.299c145.967,0,264.299-118.33,264.299-264.299c0-122.605-83.496-225.678-196.719-255.543
+							c0.217,2.207,0.334,4.443,0.334,6.708v90.292c61.441,26.42,104.58,87.531,104.58,158.542c0,95.113-77.381,172.498-172.498,172.498
+							c-95.114,0-172.499-77.383-172.499-172.498C105.785,221.256,148.928,160.148,210.37,133.725z"/>
+						<path d="M321.718,267.083V125.319V43.434c0-4.226-0.635-8.299-1.76-12.164C314.694,13.21,298.048,0,278.284,0
+							c-19.765,0-36.411,13.21-41.674,31.27c-1.126,3.862-1.759,7.938-1.759,12.164v81.886v141.767
+							c0,23.988,19.443,43.434,43.434,43.434C302.274,310.516,321.718,291.07,321.718,267.083z"/>
+					</g>
+				</g>
+			</svg>
+		</div>
+        <div class="loader-section section-left"></div>
+        <div class="loader-section section-right"></div>
+    </div>
+
+	<div id="page" class="site">
+		<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'travel' ); ?></a>
+
+		<header id="masthead" class="site-header header is-visible">
+			<div class="site-branding">
+				<?php
+				the_custom_logo();
+				?>
+			</div><!-- .site-branding if you want to add logo later -->
+
+			<nav id="site-navigation" class="main-navigation">
+				<!-- laptop and desktop menu -->
+				<?php
+				wp_nav_menu( array(
+					'theme_location' => 'menu-1',
+					'menu_id'        => 'primary-menu',
+				) );
+				?>
+
+				<!-- mobile menu with hamburger removed and triple icon layout  -->
+				<div class="mobile-menu">
+					<a class="menu-item people" href="#people">
+						<div class="menu-icon-container"><img class="people-icon menu-icon" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/people-white.png" alt="people icon" /></div>
+						<div class="menu-text">PEOPLE</div>
+					</a>
+					<a class="menu-item places" href="#places">
+						<div class="menu-icon-container"><img class="places-icon menu-icon" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/places-white.png" alt="places icon"/></div>
+						<div class="menu-text">PLACES</div>
+					</a>
+					<a class="menu-item gallery" href="#gallery">
+						<div class="menu-icon-container"><img class="gallery-icon menu-icon" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/gallery-white.png" alt="galery icon"/></div>
+						<div class="menu-text">GALLERY</div>
+					</a>
+				</div>
+
+			</nav><!-- #site-navigation -->
+		</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
